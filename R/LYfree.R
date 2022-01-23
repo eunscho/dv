ee02528f96568bb67dff3707819621da389b77bf
eunscho <bene@kw.ac.kr>
2022-01-23 20:48:44 +0900

    cc

--- R/LYfree.R
+++ R/LYfree.R
@@ -3,13 +3,28 @@ LYfree <- function(trait = 3, method = 3, item = 3) {
   for (i in 1:(trait * method * item)) {
     for (j in 1:(trait + method)) {
       if (j <= trait) { # trait loadings
-        if (ceiling(i / item) %% trait == j | ceiling(i / item) %% trait == 0) {
-          out[i, j] <- NA
+        if (j < trait) {
+          if (ceiling(i / item) %% trait == j) {
+            out[i, j] <- NA
+          } 
+        } else {
+          if (ceiling(i / item) %% trait == 0) {
+            out[i, j] <- NA
+          }
         }
       } else { # method loadings
-        
-      }
-    }
-  }
+        if (j < trait + method) {
+          if (ceiling(i / item) %% method == j - trait) {
+            out[i, j] <- NA
+          }
+        } else {
+          if (ceiling(i / item) %% method == 0) {
+            out[i, j] <- NA
+          }
+        }
+      } # end of } else { # method loadings
+    } # end of for (j in 1:(trait + method)) {
+  } # end of for (i in 1:(trait * method * item)) {
   return(out)
 }
+
