dea9405128b47d4c996bfd34297de8d97460d652
eunscho <bene@kw.ac.kr>
2022-01-25 10:28:40 +0900

    ff

--- R/campbell.R
+++ R/campbell.R
@@ -4,9 +4,10 @@ campbell <- function(r, trait, method, item) {
   for (i in 1:n) {
     for (j in 1:i) {
       if (i > j | i < j) { # nondiagonal
-        tnum <- mydiv(ceiling(i / item), trait)
-        mnum <- ceiling(i / (item * trait))
-        if (tnum == mnum) { # htmm matrix
+        tnumi <- ceiling(i / trait)
+        tnumj <- ceiling(j / trait)
+        #mnum <- ceiling(i / trait)
+        if (tnumi == tnumj) { # htmm matrix
           htmm[i, j] <- r[i, j]
         } else { # hthm matrix
           if (mydiv(i, trait) == mydiv(j, trait)) { #diagonal of the hthm matrix
@@ -14,10 +15,10 @@ campbell <- function(r, trait, method, item) {
           } else {
             hthm[i, j] <- r[i, j]
           }
-        }
-      }
-    }
-  }
+        } #  } else { # hthm matrix
+      } # if (i > j | i < j) { # nondiagonal
+    } # for (j in 1:i) {
+  } # for (i in 1:n) {
   out <- list(mthm = mthm, hthm = hthm, htmm = htmm)
   return(out)
 }
--- misc/practice.R
+++ misc/practice.R
@@ -1,7 +1,7 @@
 library(simsem)
-t <- 4
+t <- 3
 m <- 3
-k <- 2
+k <- 3
 tloading <- rep(.6, t * m * k)
 mloading <- rep(.5, t * m * k)
 LY <- bind(LYfree(t, m, k), LYpop(t, m, k, tloading, mloading))
@@ -19,21 +19,15 @@ dat <- generate(mod, 200)
 dat
 out <- analyze(model = mod, dat)
 
-?sim
-lavInspect(out, what = "est")
 
 PScon <- binds(PSfree_constrained(t, m), PSpop_constrained(t, m, tcor = tcor, mcor))
 PScon
-?model.cfa
 
 com <- composite(dat, t, m, k)
 
 dat
 sum(dat[1, 1:2])
-test[1,1]
-
-
-LYfree()
 
 r <- cor(com)
 r
+campbell(r, t, m, k)
